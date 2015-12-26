3fe318ccc6ec440792546d21cf50da083ff9c74c
Luis Correia <buga@loide.net>
2015-12-26 11:37:56 +0000

    add test code for rotary encoder and set temperature

--- SolderingStation/SolderingStation.ino
+++ SolderingStation/SolderingStation.ino
@@ -25,6 +25,10 @@ int rotaryA = 9;
 int rotaryB = 10;
 RotaryEncoder encoder(rotaryA, rotaryB);
 
+// Variables
+static int pos = 0;
+int newPos = 0;
+int setTemperature = 25;
 
 void setup() {
 
@@ -33,12 +37,20 @@ void setup() {
 
   // initialize the lcd 
   lcd.init();                      
-  // Print a message to the LCD.
   lcd.backlight();
+  // Print a message to the LCD.
   lcd.setCursor(0,0);
   lcd.print("SolderingStation");
   lcd.setCursor(0,1);
   lcd.print("v 0.1");
+
+  // pullup for Rotary Encoder pins
+  pinMode(rotaryA,INPUT_PULLUP);
+  pinMode(rotaryB,INPUT_PULLUP);
+  
+  Serial.begin(115200);
+  while (!Serial);             // Leonardo: wait for serial monitor
+  Serial.println("Soldering Station v0.1");
 
 
 }
@@ -46,17 +58,19 @@ void setup() {
 void loop() {
 
   // Get RotaryEncoder movement
-  static int pos = 0;
+  // This sets new temperature setpoint
   encoder.tick();
-
-  int newPos = encoder.getPosition();
-  if (pos != newPos) {
-    lcd.setCursor(0, 1); 
-    lcd.print(newPos);
-    lcd.print(" ");
+  newPos = encoder.getPosition();
+  if (newPos < pos){
+    setTemperature++;
     pos = newPos;
-  } // if
-
-
-
+  }
+  if (newPos > pos){
+    if (setTemperature >= 25){
+      setTemperature--;
+    }
+    pos = newPos;
+  }
+  Serial.println(setTemperature);
+  
 }
