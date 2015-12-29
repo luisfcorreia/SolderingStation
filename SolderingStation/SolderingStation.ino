429d929a0923c069105c44e3f5a51d941aed9592
Luis Correia <buga@loide.net>
2015-12-29 00:01:10 +0000

    change lcd for 7segment, still not working properly, display flickers while reading temperature off MAX31855

--- SolderingStation/SolderingStation.ino
+++ SolderingStation/SolderingStation.ino
@@ -1,6 +1,5 @@
 #include <Wire.h>
-#include "LiquidCrystal_I2C.h"
-
+#include "SevSeg.h"
 #include "Adafruit_MAX31855.h"
 #include "PID_v1.h"
 #include "ClickEncoder.h"
@@ -11,15 +10,36 @@ int thermoCS1 = 8;
 int thermoCLK = 9;
 int thermoDO  = 7;
 Adafruit_MAX31855 tc(thermoCLK, thermoCS1, thermoDO);
+double Celsius = 25;
+unsigned long tempTime;
+int tempLapse = 1500;
 
-//LCD address is 0x27 for a 16 chars and 2 line display
-int lcdAddress = 0x27;
-LiquidCrystal_I2C lcd(lcdAddress, 16, 2);
+//7Segment 3 digit thing
+SevSeg myDisplay;
+int displayType = COMMON_ANODE;
+char tempString[10];
+
+//This pinout is for a 3361BS (common anode)
+int digit1 = 21; //Pin 1
+int digit2 = 14; //Pin 10
+int digit3 = 10; //Pin 4
+int digit4 = 34; //Pin 6 (ignored)
+
+//Declare what pins are connected to the segments (cathodes)
+int segA = 19; //Pin 12
+int segB = 3; //Pin 11
+int segC = 16; //Pin 3
+int segD = 18; //Pin 8
+int segE = 20; //Pin 2
+int segF = 15; //Pin 9
+int segG = 2; //Pin 7
+int segDP = 34; //Pin 5 (ignored)
+int numberOfDigits = 3; //Do you have a 1, 2 or 4 digit display?
 
 //Rotary Encoder
-int rotaryA = 14;
-int rotaryB = 15;
-int rotaryP = 16;
+int rotaryA = 0;
+int rotaryB = 1;
+int rotaryP = 5;
 ClickEncoder *encoder;
 
 //Relay and PID
@@ -31,11 +51,10 @@ PID myPID(&Input, &Output, &Setpoint, 2, 5, 1, DIRECT);
 
 // Variables
 int newPos = 0;
-int pos = 30;
-int coolTemp = 30;
+int pos = 80;
+int coolTemp = 80;
 int maxTemp = 450;
 int setTemperature = coolTemp;
-String texto = "";
 
 // ISR for ClickEncoder
 void rotary() {
@@ -50,13 +69,9 @@ void setup() {
   myPID.SetOutputLimits(0, WindowSize);
   myPID.SetMode(AUTOMATIC);
 
-  // initialize the lcd
-  lcd.init();
-  // Print a message to the LCD.
-  lcd.setCursor(0, 0);
-  lcd.print("SolderingStation");
-  lcd.setCursor(0, 1);
-  lcd.print("v 0.1");
+  // initialize 7 segment
+  myDisplay.Begin(displayType, numberOfDigits, digit1, digit2, digit3, digit4, segA, segB, segC, segD, segE, segF, segG, segDP);
+  myDisplay.SetBrightness(80); //Set the display to 100% brightness level
 
   // Rotary Encoder pins
   encoder = new ClickEncoder(rotaryA, rotaryB, rotaryP);
@@ -67,15 +82,13 @@ void setup() {
   Timer1.initialize(1000);
   Timer1.attachInterrupt(rotary);
 
+  // Timer for temperature reading
+  tempTime = millis();
   Serial.begin(115200);
-  while (!Serial);             // Leonardo: wait for serial monitor
-  texto = "Soldering Station v0.1";
-  Serial.println(texto);
 
 }
 
 void loop() {
-  texto = "";
 
   // Get RotaryEncoder value
   newPos += encoder->getValue();
@@ -84,25 +97,29 @@ void loop() {
     if (setTemperature <= coolTemp) {
       setTemperature = coolTemp;
     }
-    Serial.print("New Temperature ");
-    Serial.println(setTemperature);
   }
   if (newPos > pos) {
     setTemperature = setTemperature + 10;
     if (setTemperature >= maxTemp) {
       setTemperature = maxTemp;
     }
-    Serial.print("New Temperature ");
-    Serial.println(setTemperature);
   }
   pos = newPos;
 
-  // read Temperature
-  Input = tc.readCelsius();
-  Setpoint = setTemperature;
-  
+  if (millis() >= tempTime) {
+    tempTime = millis() + tempLapse;
+    // read Temperature
+    Celsius = tc.readCelsius();
+    Serial.print ("Temp: ");
+    Serial.println (Celsius);
+    Input = Celsius;
+    Setpoint = setTemperature;
+  }
+
   myPID.Compute();
 
+  sprintf(tempString, "%03d", setTemperature);
+  myDisplay.DisplayString(tempString, 4);
 
   /************************************************
      turn the output pin on/off based on pid output
@@ -113,16 +130,14 @@ void loop() {
   }
   if (Output < millis() - windowStartTime) {
     digitalWrite(RelayPin, HIGH);
-    Serial.print ("Temp: ");
-    Serial.print (Input);
-    Serial.println(" Relay ON");
+    //    Serial.print ("Temp: ");
+    //    Serial.print (Input);
+    //    Serial.println(" Relay ON");
   }
   else {
     digitalWrite(RelayPin, LOW);
-    Serial.print ("Temp: ");
-    Serial.print (Input);
-    Serial.println(" Relay OFF");
+    //    Serial.print ("Temp: ");
+    //    Serial.print (Input);
+    //    Serial.println(" Relay OFF");
   }
-
-
 }
